b6236a8ffa306d657309a74e0a6e3fc034781940
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-11-25 15:22:55 +0900

    Initial commit

---
+++ src/main/java/spring_practice/RazExpress/RazExpressApplication.java
@@ -0,0 +1,13 @@
+package spring_practice.RazExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring_practice/RazExpress/RazExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_practice.RazExpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
