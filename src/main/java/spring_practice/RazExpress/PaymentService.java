c4b4d88a8b164893c4ccd31d2e1c35eb95837880
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-11-25 16:40:44 +0900

    refactor(PaymentService): parpare() 메서드에 파라미터 추가
    - 간단하게 설계된 Payment의 생성자에 파라미터 전달

--- src/main/java/spring_practice/RazExpress/PaymentService.java
+++ src/main/java/spring_practice/RazExpress/PaymentService.java
@@ -1,14 +1,17 @@
 package spring_practice.RazExpress;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentService {
 
-    public Payment prepare() {
-        return new Payment();
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now());
     }
 
     public static void main(String[] args) {
         PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare();
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.valueOf(150.8));
         System.out.println(payment);
     }
 }
