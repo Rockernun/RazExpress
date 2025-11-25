727d31b5e557bcee5a668b1df97cb02010a09b48
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-11-25 17:15:50 +0900

    refactor(PaymentService): 환율 정보를 받아서 원화 금액으로 환산 및 환율 유효 시간 설정
    - https://open.er-api.com/v6/latest의 실시간 환율 정보를 이용하여 원화 금액 계산
    - 원화 환산 금액 유효 시간을 10분으로 설정

--- src/main/java/spring_practice/RazExpress/PaymentService.java
+++ src/main/java/spring_practice/RazExpress/PaymentService.java
@@ -1,15 +1,35 @@
 package spring_practice.RazExpress;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+import tools.jackson.databind.ObjectMapper;
 
 public class PaymentService {
 
-    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
-        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now());
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = bufferedReader.lines().collect(Collectors.joining());
+        bufferedReader.close();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ExchangeRateData exchangeRateData = objectMapper.readValue(response, ExchangeRateData.class);
+        BigDecimal exRate = exchangeRateData.rates().get("KRW");
+
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(10);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         PaymentService paymentService = new PaymentService();
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.valueOf(150.8));
         System.out.println(payment);
