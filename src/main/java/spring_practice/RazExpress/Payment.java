26ec9ddf861a7e02e3849840052d14f889247ec6
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-11-25 16:13:08 +0900

    feat(PaymentService): prepare() 메서드 추가
    - 요구 사항에 맞게 Payment 인스턴스를 반환하는 prepare() 메서드 추가
    - 정상적으로 동작하는지 prepare() 메서드 실행

---
+++ src/main/java/spring_practice/RazExpress/Payment.java
@@ -0,0 +1,4 @@
+package spring_practice.RazExpress;
+
+public class Payment {
+}
---
+++ src/main/java/spring_practice/RazExpress/PaymentService.java
@@ -0,0 +1,14 @@
+package spring_practice.RazExpress;
+
+public class PaymentService {
+
+    public Payment prepare() {
+        return new Payment();
+    }
+
+    public static void main(String[] args) {
+        PaymentService paymentService = new PaymentService();
+        Payment payment = paymentService.prepare();
+        System.out.println(payment);
+    }
+}
