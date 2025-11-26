45997f41df4cd204386652ac240724f11334748a
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-11-26 14:23:02 +0900

    refactor(PaymentService): 원화(KRW) 환율 조회 로직을 getKrwExRate 메서드로 분리
    - prepare 메서드는 금액 환산과 유효기간 계산에만 집중하도록 역할 단순화
    - 환율을 받아오는 정보나 환율을 적용할 통화의 종류를 바꾸고 싶다면 getKrwExRate() 메서드만 수정(관심사의 분리)

--- src/main/java/spring_practice/RazExpress/PaymentService.java
+++ src/main/java/spring_practice/RazExpress/PaymentService.java
@@ -13,6 +13,14 @@ import tools.jackson.databind.ObjectMapper;
 public class PaymentService {
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
+        BigDecimal exRate = getKrwExRate(currency);
+        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
+        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(10);
+
+        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+    }
+
+    private BigDecimal getKrwExRate(String currency) throws IOException {
         URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -22,11 +30,7 @@ public class PaymentService {
         ObjectMapper objectMapper = new ObjectMapper();
         ExchangeRateData exchangeRateData = objectMapper.readValue(response, ExchangeRateData.class);
         BigDecimal exRate = exchangeRateData.rates().get("KRW");
-
-        BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
-        LocalDateTime validUntil = LocalDateTime.now().plusMinutes(10);
-
-        return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
+        return exRate;
     }
 
     public static void main(String[] args) throws IOException {
