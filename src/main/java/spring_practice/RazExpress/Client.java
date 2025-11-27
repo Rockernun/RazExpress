856070d7140d6450395c718c0cdd9c70ad90bda8
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-11-27 14:30:20 +0900

    refactor(Client): Client가 스프링 컨테이너에서 PaymentService를 조회하도록 변경
    - AnnotationConfigApplicationContext로 ObjectFactory 설정 클래스를 로딩
    - BeanFactory에서 PaymentService 빈을 조회

--- src/main/java/spring_practice/RazExpress/Client.java
+++ src/main/java/spring_practice/RazExpress/Client.java
@@ -2,11 +2,13 @@ package spring_practice.RazExpress;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        ObjectFactory objectFactory = new ObjectFactory();
-        PaymentService paymentService = objectFactory.paymentService();
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.valueOf(150.8));
         System.out.println(payment);
