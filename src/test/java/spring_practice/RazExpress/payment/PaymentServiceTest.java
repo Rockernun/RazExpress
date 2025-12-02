a8c45b7c63c7ae3c371f518abfcafa5f4eda9321
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-12-02 17:16:13 +0900

    test(PaymentServiceTest): PaymentService 단위 테스트에서 고정 Clock을 사용해 유효 시간 검증 추가
    - @BeforeEach에서 Clock.fixed(...)로 고정된 Clock을 초기화해 테스트마다 동일한 기준 시각 사용
    - validUntil 테스트를 추가해 고정 Clock 기준 now로부터 30분이 정확히 설정되는지 검증

--- src/test/java/spring_practice/RazExpress/payment/PaymentServiceTest.java
+++ src/test/java/spring_practice/RazExpress/payment/PaymentServiceTest.java
@@ -2,26 +2,44 @@ package spring_practice.RazExpress.payment;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import spring_practice.RazExpress.exrate.WebApiExRateProvider;
 
 class PaymentServiceTest {
 
-    @Test
-    @DisplayName("prepare() 메서드가 요구 사항 3가지를 충족했는지 검증")
-    void prepare() throws IOException {
-        getPayment(BigDecimal.valueOf(500), BigDecimal.valueOf(5_000));
-        getPayment(BigDecimal.valueOf(1_000), BigDecimal.valueOf(10_000));
-        getPayment(BigDecimal.valueOf(3_000), BigDecimal.valueOf(30_000));
-//        Assertions.assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-//        Assertions.assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    Clock clock;
+
+    @BeforeEach
+    void setUp() {
+        this.clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
     }
 
-    private static void getPayment(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
-        PaymentService service = new PaymentService(new ExRateProviderStub(exRate));
+    @Test
+     void prepare() throws IOException {
+        getPayment(BigDecimal.valueOf(500), BigDecimal.valueOf(5_000), clock);
+        getPayment(BigDecimal.valueOf(1_000), BigDecimal.valueOf(10_000), clock);
+        getPayment(BigDecimal.valueOf(3_000), BigDecimal.valueOf(30_000), clock);
+    }
+
+    @Test
+    void validUntil() throws IOException {
+        PaymentService service = new PaymentService(new ExRateProviderStub(BigDecimal.valueOf(1_000)), clock);
+
+        Payment payment = service.prepare(1L, "USD", BigDecimal.TEN);
+
+        LocalDateTime now = LocalDateTime.now(this.clock);
+        LocalDateTime expectedValidUntil = now.plusMinutes(30);
+
+        Assertions.assertThat(payment.getValidUntil()).isEqualTo(expectedValidUntil);
+    }
+
+    private static void getPayment(BigDecimal exRate, BigDecimal convertedAmount, Clock clock) throws IOException {
+        PaymentService service = new PaymentService(new ExRateProviderStub(exRate), clock);
 
         Payment payment = service.prepare(1L, "USD", BigDecimal.TEN);
 
