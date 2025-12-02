7a1fec658a9927178f8d007cc084949d0d098049
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-12-02 17:13:41 +0900

    refactor(TestPaymentConfig): 테스트 전용 Clock과 ExRateProviderStub을 사용하는 TestPaymentConfig 추가

--- src/test/java/spring_practice/RazExpress/TestPaymentConfig.java
+++ src/test/java/spring_practice/RazExpress/TestPaymentConfig.java
@@ -1,23 +1,30 @@
 package spring_practice.RazExpress;
 
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import spring_practice.RazExpress.exrate.WebApiExRateProvider;
 import spring_practice.RazExpress.payment.ExRateProvider;
 import spring_practice.RazExpress.payment.ExRateProviderStub;
 import spring_practice.RazExpress.payment.PaymentService;
 
 @Configuration
-public class TestObjectFactory {
+public class TestPaymentConfig {
 
     @Bean
     public PaymentService paymentService() {
-        return new PaymentService(exRateProvider());
+        return new PaymentService(exRateProvider(), clock());
     }
 
     @Bean
     public ExRateProvider exRateProvider() {
         return new ExRateProviderStub(BigDecimal.valueOf(1_000));
+    }
+
+    @Bean
+    public Clock clock() {
+        return Clock.fixed(Instant.now(), ZoneId.systemDefault());
     }
 }
