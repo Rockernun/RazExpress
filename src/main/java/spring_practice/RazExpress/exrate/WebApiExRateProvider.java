b22bc94f7a3db0f708ec2eb55bcd038df95e41c7
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-12-08 16:46:53 +0900

    refactor(WebApiExRateProvider): 예외 처리 및 HTTP 요청 로직 개선

--- src/main/java/spring_practice/RazExpress/exrate/WebApiExRateProvider.java
+++ src/main/java/spring_practice/RazExpress/exrate/WebApiExRateProvider.java
@@ -5,25 +5,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.stream.Collectors;
+import org.springframework.boot.json.JsonParseException;
 import spring_practice.RazExpress.payment.ExRateProvider;
 import tools.jackson.databind.ObjectMapper;
 
 public class WebApiExRateProvider implements ExRateProvider {
-
     @Override
-    public BigDecimal getExRate(String currency) throws IOException {
-        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String response = bufferedReader.lines().collect(Collectors.joining());
-        bufferedReader.close();
+    public BigDecimal getExRate(String currency) {
+        String url = "https://open.er-api.com/v6/latest/" + currency;
+
+        URI uri;
+        try {
+            uri = new URI(url);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        String response;
+        try {
+            HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
+            try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                response = bufferedReader.lines().collect(Collectors.joining());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         ObjectMapper objectMapper = new ObjectMapper();
-        ExchangeRateData exchangeRateData = objectMapper.readValue(response, ExchangeRateData.class);
-
-        System.out.println("API ExRate: " + exchangeRateData.rates().get("KRW"));
-        return exchangeRateData.rates().get("KRW");
+        try {
+            ExchangeRateData exchangeRateData = objectMapper.readValue(response, ExchangeRateData.class);
+            return exchangeRateData.rates().get("KRW");
+        } catch (JsonParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
