13ad0a31dc36125400879980657abbf6425912cb
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-12-09 14:35:14 +0900

    refactor(ObjectFactory): ApiTemplate 및 WebApiExRateProvider를 스프링 빈으로 구성

--- src/main/java/spring_practice/RazExpress/ObjectFactory.java
+++ src/main/java/spring_practice/RazExpress/ObjectFactory.java
@@ -3,6 +3,9 @@ package spring_practice.RazExpress;
 import java.time.Clock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import spring_practice.RazExpress.api.ApiTemplate;
+import spring_practice.RazExpress.api.ErApiExRateExtractor;
+import spring_practice.RazExpress.api.SimpleApiExecutor;
 import spring_practice.RazExpress.payment.ExRateProvider;
 import spring_practice.RazExpress.exrate.WebApiExRateProvider;
 import spring_practice.RazExpress.payment.PaymentService;
@@ -16,8 +19,13 @@ public class ObjectFactory {
     }
 
     @Bean
+    public ApiTemplate apiTemplate() {
+        return new ApiTemplate(new SimpleApiExecutor(), new ErApiExRateExtractor());
+    }
+
+    @Bean
     public ExRateProvider exRateProvider() {
-        return new WebApiExRateProvider();
+        return new WebApiExRateProvider(apiTemplate());
     }
 
     @Bean
