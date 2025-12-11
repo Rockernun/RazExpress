e67355e0c46faf048b30f5d8eac80b1cacf0699a
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-12-11 17:39:17 +0900

    refactor(DataConfig): JPA 트랜잭션 및 @PersistenceContext 지원 설정 추가
    - PersistenceAnnotationBeanPostProcessor 빈 등록으로
      @PersistenceContext가 붙은 필드에 EntityManager를 주입할 수 있도록 설정
    - EntityManagerFactory를 주입받는 JpaTransactionManager 빈을 등록해
      JPA 기반 트랜잭션 관리를 활성화
    - OrderRepository를 스프링 빈으로 등록해 DataClient 등에서 DI로 사용할 수 있도록 구성

--- src/main/java/spring_practice/RazExpress/DataConfig.java
+++ src/main/java/spring_practice/RazExpress/DataConfig.java
@@ -1,13 +1,18 @@
 package spring_practice.RazExpress;
 
+import jakarta.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import spring_practice.RazExpress.data.OrderRepository;
 
 @Configuration
 public class DataConfig {
@@ -29,5 +34,20 @@ public class DataConfig {
         }});
 
         return emf;
+    }
+
+    @Bean
+    public BeanPostProcessor persistenceAnnotationBeanPostProcessor() {
+        return new PersistenceAnnotationBeanPostProcessor();
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
+    @Bean
+    public OrderRepository orderRepository() {
+        return new OrderRepository();
     }
 }
