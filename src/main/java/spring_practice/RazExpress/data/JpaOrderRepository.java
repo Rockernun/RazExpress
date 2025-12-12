5944bdcf593304ea2bb6db21edbb2872746be85e
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-12-12 11:42:50 +0900

    refactor(JpaOrderRepository): 기존 OrderRepository를 JpaOrderRepository로 변경해 구현 클래스로 명확히 분리

--- src/main/java/spring_practice/RazExpress/data/JpaOrderRepository.java
+++ src/main/java/spring_practice/RazExpress/data/JpaOrderRepository.java
@@ -3,12 +3,14 @@ package spring_practice.RazExpress.data;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import spring_practice.RazExpress.order.Order;
+import spring_practice.RazExpress.order.OrderRepository;
 
-public class OrderRepository {
+public class JpaOrderRepository implements OrderRepository {
 
     @PersistenceContext
     private EntityManager em;
 
+    @Override
     public void save(Order order) {
         em.persist(order);
     }
