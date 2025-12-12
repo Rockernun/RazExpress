3182c54917e85cfdf5052d7d058f2f6e02946e16
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-12-12 23:05:31 +0900

    refactor(OrderConfig): OrderService 구성을 트랜잭션 프록시 기반으로 변경

--- src/main/java/spring_practice/RazExpress/order/OrderConfig.java
+++ src/main/java/spring_practice/RazExpress/order/OrderConfig.java
@@ -1,23 +1,30 @@
 package spring_practice.RazExpress.order;
 
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import spring_practice.RazExpress.DataConfig;
-import spring_practice.RazExpress.data.OrderRepository;
+import spring_practice.RazExpress.data.JdbcOrderRepository;
 
 @Configuration
 @Import(DataConfig.class)
 public class OrderConfig {
 
     @Bean
-    public OrderRepository orderRepository() {
-        return new OrderRepository();
+    public OrderRepository orderRepository(DataSource dataSource) {
+        return new JdbcOrderRepository(dataSource);
     }
 
     @Bean
-    public OrderService orderService(JpaTransactionManager transactionManager) {
-        return new OrderService(orderRepository(), transactionManager);
+    public OrderService orderService(
+            PlatformTransactionManager transactionManager,
+            OrderRepository orderRepository
+    ) {
+        return new OrderServiceTxProxy(
+                new OrderServiceImpl(orderRepository),
+                transactionManager
+        );
     }
 }
